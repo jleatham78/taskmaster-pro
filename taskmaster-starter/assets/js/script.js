36808fa654016e432e69bbfe34a28371d6720e3f
jleatham78 <janeleleatham01@gmail.com>
2020-12-03 18:54:30 -0700

    UX enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -172,16 +172,24 @@ $(".list-group").on("change", "input[type='text']", function() {
     tolerance: "pointer",
     helper: "clone",
     activate: function(event) {
-      console.log("activate", this);
+      $(this).addClass("dropover");
+      $(".bottom-trash").addClass("bottom-trash-drag");
+     //console.log("activate", this);
     },
     deactivate: function(event) {
-      console.log("deactivate", this);
+      $(this).removeClass("dropover");
+      $(".bottom-trash").removeClass("bottom-trash-drag");
+      //console.log("deactivate", this);
     },
     over: function(event) {
-      console.log("over", event.target);
+      $(this).addClass("dropover-active");
+      $(".bottom-trash").addClass("bottom-trash-active");
+      //console.log("over", event.target);
     },
     out: function(event) {
-      console.log("out", event.target);
+      $(this).removeClass("dropover-active");
+      $(".bottom-trash").removeClass("bottom-trash-active");
+      //console.log("out", event.target);
     },
     update: function(event) {
 
@@ -224,13 +232,8 @@ $(".list-group").on("change", "input[type='text']", function() {
     tolerance: "touch",
     drop: function(event, ui) {
       ui.draggable.remove();
+      $("#bottom-trash").removeClass("bottom-trash-active");
       console.log("drop");
-    },
-    over: function(event, ui) {
-      console.log("over");
-    },
-    out: function(event, ui) {
-      console.log("out");
     }
   })
 
@@ -251,7 +254,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -287,5 +290,11 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 
